b167ca4c4bf4d265ed15f517eb6e6cee0f9ed3b8
Stefano Vazzoler <stefanovazzocell@gmail.com>
2018-06-02 16:39:51 -0700

    Prepared main constructor

--- Source/njq.js
+++ Source/njq.js
@@ -1,2 +1,21 @@
 "use strict";
 
+/*
+* NJQ (Version 0.0.0)
+* by Stefano Vazzoler (stefanovazzocell@gmail.com)
+* https://stefanovazzoler.com/
+*/
+
+(function(window,document,undefined){
+	var $$ = function(selector) {
+		if (!(this instanceof $$)) // If it's a new instance,
+			return new $$(selector); // initialize it with the given selector
+		if (typeof selector == 'string') { // If selector has string
+			this.selector = document.querySelectorAll(selector); // Attempt to find it
+		} else this.selector = selector; // Else, just assume it to be a NodeList
+	};
+	window.$$ = $$;
+	$$.fn = $$.prototype = {
+		// Main functionality goes here
+	};
+})(this,document);
